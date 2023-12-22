5cc984442171431a3c1142a6048e26db80b4e7b1
charlineBx <charline.beaudeux8@etu.univ-lorraine.fr>
2023-12-22 11:12:08 +0100

    init projet

---
+++ BACKEND/config.js
@@ -0,0 +1,10 @@
+module.exports =  {
+    ACCESS_TOKEN_SECRET : "EMMA123",
+    BDD : {
+        "host" :"dpg-cm23hg6n7f5s73erf12g-a.oregon-postgres.render.com",
+        "port" : "5432",
+        "user" : "bdd_catalogue_user",
+        "password" : "MXWOilrWSOeapjX3ofjMKd27ZnR3ewUF",
+        "bdname" :"bdd_catalogue" 
+        }
+}
---
+++ FRONTEND/src/app/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  backendLoginClient: 'https://projet-backend-frjs.onrender.com/api/utilisateur/login',
+  backendProduit: 'https://projet-backend-frjs.onrender.com/api/catalogue',
+};
